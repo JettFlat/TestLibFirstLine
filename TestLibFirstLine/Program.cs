db14b95a4959e8269e2b476a797e0b4e7c51a766
Den <backupper97@gmail.com>
2020-09-17 17:59:21 +0300

    Добавьте файлы проекта.

---
+++ TestLibFirstLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestLibFirstLine
+{
+    public class Triangle
+    {
+        public enum TriagType
+        {
+            Right,
+            AcuteAngled,
+            Obtuse,
+            NotTriangle
+        }
+        static public TriagType GetTriagType(double a, double b, double c)
+        {
+            if (a + b > c && a + c > b && b + c > a)
+            {
+                var Sides = new List<double> { a, b, c };
+                var MaxSide = Sides.Where(x => Sides.Where(y => y < x).Count() == 2).FirstOrDefault();
+                double OtherSidesKvSum = 0;
+                Sides.Where(y => y != MaxSide).ToList().ForEach(x => OtherSidesKvSum += x * x);
+                int MaxSideKv = (int)(MaxSide * MaxSide);
+                if (MaxSideKv == (int)OtherSidesKvSum)
+                    return TriagType.Right;
+                if (MaxSideKv < (int)OtherSidesKvSum)
+                    return TriagType.AcuteAngled;
+                if (MaxSideKv > (int)OtherSidesKvSum)
+                    return TriagType.Obtuse;
+            }
+            return TriagType.NotTriangle;
+        }
+    }
+}
---
+++ TestLibFirstLineTests/TriangleTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestLibFirstLine;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestLibFirstLine.Tests
+{
+    [TestClass()]
+    public class TriangleTests
+    {
+        [TestMethod()]
+        public void GetTriagTypeTest_21_21_29p7_right_returned()
+        {
+            double a = 29.7;
+            double b = 21;
+            double c = 21;
+            Triangle.TriagType expected = Triangle.TriagType.Right;
+            Triangle.TriagType actual = Triangle.GetTriagType(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetTriagTypeTest_3_4_5_right_returned()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            Triangle.TriagType expected = Triangle.TriagType.Right;
+            Triangle.TriagType actual = Triangle.GetTriagType(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetTriagTypeTest_8p97_10_7p32_AcuteAngled_returned()
+        {
+            double a = 8.97;
+            double b = 10;
+            double c = 7.32;
+            Triangle.TriagType expected = Triangle.TriagType.AcuteAngled;
+            Triangle.TriagType actual = Triangle.GetTriagType(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetTriagTypeTest_10_4p62_12p94_Obtuse_returned()
+        {
+            double a = 10;
+            double b = 4.62;
+            double c = 12.94;
+            Triangle.TriagType expected = Triangle.TriagType.Obtuse;
+            Triangle.TriagType actual = Triangle.GetTriagType(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void GetTriagTypeTest_5_5_20_NotTriangle_returned()
+        {
+            double a = 5;
+            double b = 5;
+            double c = 20;
+            Triangle.TriagType expected = Triangle.TriagType.NotTriangle;
+            Triangle.TriagType actual = Triangle.GetTriagType(a, b, c);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
